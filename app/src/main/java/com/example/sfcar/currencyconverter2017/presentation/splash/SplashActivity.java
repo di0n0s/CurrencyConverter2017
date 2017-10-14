0c38fcbab3da6056df93874ba4daf0c10d222218
sfuentes <sfuentes@opentrends.net>
2017-10-14 12:27:00 +0200

    [WIP] SplashActivity

---
+++ app/src/main/java/com/example/sfcar/currencyconverter2017/presentation/splash/SplashActivity.java
@@ -0,0 +1,12 @@
+package com.example.sfcar.currencyconverter2017.presentation.splash;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
